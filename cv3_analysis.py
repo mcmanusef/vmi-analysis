042621c45811d1fcffccd1792b3574e98d48e3ef
Edward Fox <mcmanusef@gmail.com>
2023-01-14 15:29:00 -0500

    Made preliminary cv3 histogram program

---
+++ cv3_analysis.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 13 22:08:56 2023
+
+@author: mcman
+"""
+
+import h5py
+import numpy as np
+import functools
+import itertools
+import matplotlib.pyplot as plt
+from numba import njit, vectorize
+
+
+def correlate_tof(data_iter, tof_data):
+    dc = next(data_iter, None)
+    tc = next(tof_data, None)
+    while not (dc is None or tc is None):
+        if dc[0] > tc[0]:
+            tc = next(tof_data, None)
+        elif dc[0] < tc[0]:
+            dc = next(data_iter, None)
+        else:
+            yield (dc[0], tuple(list(dc[1])+[tc[1]]))
+            tc = next(tof_data, None)
+
+
+@njit(cache=True)
+def dist(x, y, x0, y0):
+    return np.sqrt((x-x0)**2+(y-y0)**2)
+
+
+@njit(cache=True)
+def in_good_pixels(coords):
+    x, y, t = coords
+    conditions = np.array([dist(x, y, 195, 234) < 1.5,
+                           dist(x, y, 203, 185) < 1.5,
+                           dist(x, y, 253, 110) < 1.5,
+                           dist(x, y,  23, 255) < 1.5,
+                           dist(x, y, 204, 187) < 1.5,
+                           dist(x, y,  98, 163) < 1.5])
+    return not np.any(conditions)
+
+
+@vectorize
+def P_xy(x):
+    return (np.sqrt(5.8029e-4)*(x))*np.sqrt(2*0.03675)
+
+
+@vectorize
+def P_z(t):
+    Ez = ((6.7984E-05*t**4+5.42E-04*t**3+1.09E-01*t**2)*(t < 0) +
+          (-5.64489E-05*t**4+3.37E-03*t**3-6.94E-02*t**2)*(t > 0))
+    # Ez = 0.074850168*t**2*(t < 0)-0.034706593*t**2*(t > 0)+3.4926E-05*t**4*(t > 0)  # old
+    return np.sqrt(np.abs(Ez))*((Ez > 0)+0-(Ez < 0))*np.sqrt(2*0.03675)
+
+
+@njit(cache=True)
+def smear(x, amount=0.26):
+    return x/1000+np.random.rand()*amount
+
+
+@njit
+def momentum_conversion(coords):
+    x, y, t = coords
+    return P_xy(x), P_xy(y), P_z(t)
+
+
+@njit
+def centering(x, center=(128, 128, 528.5)):
+    return (x[0]-center[0], x[1]-center[1], x[2]-center[2])
+
+
+@njit(cache=True)
+def rotate_coords(coords, theta=-1, phi=0):
+    x, y, z = coords
+    xp, yp, zp = x*np.cos(theta)+y*np.sin(theta), y*np.cos(theta)-x*np.sin(theta), z
+    return zp * np.cos(phi)+yp*np.sin(phi), yp*np.cos(phi)-zp*np.sin(phi), xp
+
+
+if __name__ == '__main__':
+    input_file = "s.cv3"
+    pol = 0
+    data = {}
+    with h5py.File(input_file) as f:
+        for k in f.keys():
+            data[k] = list(f[k][()])
+
+    __, coords = tuple(zip(*list(correlate_tof(
+        zip(data["cluster_corr"], zip(data["x"], data["y"])),
+        zip(data["etof_corr"], map(smear, list(np.array(data["t_etof"])/1000)))))))
+
+    px, py, pz = zip(*list(
+        filter(lambda x: abs(x[2]) < 0.05,
+               map(functools.partial(rotate_coords, phi=pol),
+                   map(momentum_conversion,
+                       map(centering,
+                           filter(in_good_pixels, coords)))))))
+
+    del coords, input_file, k, f, data, pol
+
+    # plt.figure(1)
+    # plt.hist2d(px, py, bins=100, range=[[-1, 1], [-1, 1]], cmap='jet')
+    # plt.figure(2)
+    # plt.hist(pz, bins=1000, range=[-1, 1])
