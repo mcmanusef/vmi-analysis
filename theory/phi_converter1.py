f7f86c00d2faab2aff7541d0ed4c3de5576a3ccc
Edward <mcmanusef@gmail.com>
2023-06-02 12:41:03 -0400

    changed files and reformatted

--- theory/phi_converter1.py
+++ theory/phi_converter1.py
@@ -14,27 +14,27 @@ def getmeans(edges):
     return np.asarray([(a+b)/2 for a, b in itertools.pairwise(edges)])
 
 
-files = ["ellip01_slices_momentum.dat",
-         "ellip03_slices_momentum.dat",
+files = ["cartesian-H-800nm-ellip06-momspe-2d-sw.dat",
          ]
 
-els = [1, 3, 4, 5, 6]
+els = [6]
 for file, el in zip(files, els):
     print(el)
-    px, py, sig, *_ = map(list, zip(*[[float(x) for x in l.split()] for l in open(file, 'r') if l.strip()]))
+    px, py, *sigs = map(list, zip(*[[float(x) for x in l.split()] for l in open(file, 'r') if l.strip()]))
 
     size = 2048  # len(set(x**2+y**2 for x, y in zip(px, py)))
     xv = np.linspace(min(px), max(px), size)
     yv = np.linspace(min(py), max(py), size)
+    for i, sig in enumerate(sigs):
+        print(i)
+        data = scipy.interpolate.griddata(
+            (np.asarray(px), np.asarray(py)),
+            sig,
+            tuple(map(np.ravel, np.meshgrid(xv, yv))),
+            fill_value=0).reshape(size, size)
 
-    data = scipy.interpolate.griddata(
-        (np.asarray(px), np.asarray(py)),
-        sig,
-        tuple(map(np.ravel, np.meshgrid(xv, yv))),
-        fill_value=0).reshape(size, size)
-
-    savemat(f"theory_0{el}.mat", {
-        "hist": np.reshape(data, (size, 1, size))**1,
-        "xv": xv,
-        "yv": yv,
-        "zv": np.array(0)})
+        savemat(f"theory_H_{i}.mat", {
+            "hist": np.reshape(data, (size, 1, size))**1,
+            "xv": xv,
+            "yv": yv,
+            "zv": np.array(0)})
