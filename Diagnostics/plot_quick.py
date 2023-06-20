6e2c890815c65a7cab22070acd5a46127b861215
Edward <mcmanusef@gmail.com>
2023-06-20 15:07:39 -0400

    adjusted usage

--- Diagnostics/plot_quick.py
+++ Diagnostics/plot_quick.py
@@ -4,6 +4,8 @@ import h5py
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+
+import cv3_analysis
 from minor_utils.tof_calibration import get_calibration
 
 from cv3_analysis import load_cv3
@@ -13,8 +15,9 @@ matplotlib.use('Qt5Agg')
 plt.close("all")
 
 wdir=r"J:\ctgroup\DATA\UCONN\VMI\VMI\20230601"
-wdir=r'C:\Users\mcman\Code\VMI\Data'
-name="kr002_s"
+# wdir=r'C:\Users\mcman\Code\VMI\Data'
+name="kr001_p"
+file=r"C:\Users\mcman\Code\VMI\Data\clust_v3\xe005_e.cv3"
 # file=os.path.join(wdir,fr"{name}.h5")
 #
 #
@@ -54,11 +57,17 @@ rt=(0,1e6)
 
 
 data2={}
-with h5py.File(os.path.join(wdir, fr"{name}.cv3")) as f:
-    for k in f.keys():
-        data2[k] = f[k][()]
-plt.figure('Clustered x-y')
-plt.hist2d(data2['x'],data2['y'],bins=256, range=((0,256),(0,256)))
+# file=os.path.join(wdir, fr"{name}.cv3")
+with h5py.File(file) as f:
+    # for k in f.keys():
+    #     data2[k] = f[k][()]
+    etof=f['t_etof'][()]/1000
+    smeared_etof=np.asarray(list(map(cv3_analysis.smear,etof)))
+    plt.figure("e-tof")
+    plt.hist(etof, bins=1000,range=(748_430,748_430+0.26*1000/5))
+#%%
+# plt.figure('Clustered x-y')
+# plt.hist2d(data2['x'],data2['y'],bins=256, range=((0,256),(0,256)))
 
 # pulse_selection = data2['tof_corr'][np.argwhere(np.logical_and(data2['t_tof']>.764e9,data2['t_tof']<.766e9))]
 # index=np.argwhere([x in pulse_selection for x in data2['cluster_corr']]).flatten()
@@ -68,21 +77,26 @@ plt.hist2d(data2['x'],data2['y'],bins=256, range=((0,256),(0,256)))
 # plt.hist2d(xf,yf,bins=256, range=((0,256),(0,256)),norm='log')
 #
 #
-xf, yf, tf = load_cv3(os.path.join(wdir, fr"{name}.cv3"),raw=True,center=(0,0,0))
+center=(125,127,528.75)
+xf, yf, tf = load_cv3(file,raw=True,center=center)
 #%%
-rt=(748_430,748_470)
+rx=ry=(-128,128)
+rt=(-10,10)
+# rx=ry=(0,256)
+# rt=(520,540)
+
 x,y,t= map(np.asarray,zip(*[(a,b,c) for a,b,c in zip(xf,yf,tf) if rx[0]<a<rx[1] and ry[0]<b<ry[1] and rt[0]<c<rt[1]]))
 
-plt.figure("e-tof")
-plt.hist(data2['t_etof']/1000, bins=1000,range=rt)
 
+
+#%%
 
 plt.figure("x-y")
-plt.hist2d(x,y,bins=512, range=(rx,ry),norm='log')
+plt.hist2d(x,y,bins=512, range=(rx,ry),norm=matplotlib.colors.PowerNorm(0.5))
 plt.figure("x-t")
-plt.hist2d(x,t,bins=512, range=(rx,rt))
+plt.hist2d(x,t,bins=256, range=(rx,rt))
 plt.figure("y-t")
-plt.hist2d(y,t,bins=512,range=(ry,rt))
+plt.hist2d(y,t,bins=256,range=(ry,rt))
 
 #%%
 with h5py.File(os.path.join(wdir, fr"{name}.cv3")) as f:
