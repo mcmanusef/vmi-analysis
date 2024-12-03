44c3542299184769041a2cb1476a33ced124ee67
mcman <mcmanusef@gmail.com>
2024-12-03 11:55:12 -0500

    added diagnostics

---
+++ clustering_.py
@@ -0,0 +1,47 @@
+from vmi_analysis.processing.pipelines import base_pipeline, run_pipeline
+from vmi_analysis.processing.processes import *
+from vmi_analysis.processing import data_types
+
+class PrintingProcess(AnalysisStep):
+    def __init__(self, queue):
+        super().__init__()
+        self.queue = queue
+        self.input_queues = [queue]
+
+    def action(self):
+        if not self.queue.empty():
+            out= self.queue.get()
+            if out[1][0]>20000:
+                print(out)
+
+class ClusterTest(base_pipeline.AnalysisPipeline):
+    def __init__(self, fname):
+        super().__init__()
+        self.queues = {
+            "chunk": data_types.ExtendedQueue(maxsize=1000),
+            "pixel": data_types.ExtendedQueue(),
+            "etof": data_types.ExtendedQueue(force_monotone=True, maxsize=50000),
+            "itof": data_types.ExtendedQueue(force_monotone=True, maxsize=50000),
+            "pulses": data_types.ExtendedQueue(force_monotone=True, maxsize=50000),
+            "clusters": data_types.ExtendedQueue(force_monotone=True, maxsize=50000),
+
+            "t_etof": data_types.ExtendedQueue(names=("etof_corr", ("t_etof",)), dtypes=('i', ('f',))),
+            "t_itof": data_types.ExtendedQueue(names=("itof_corr", ("t_itof",)), dtypes=('i', ('f',))),
+            "t_pulse": data_types.ExtendedQueue(names=("t_pulse",),dtypes=('i',)),
+            "t_cluster": data_types.ExtendedQueue(names=("clust_corr", ("t", "x", "y")), dtypes=('i', ('f', 'f', 'f'))),
+        }
+        self.processes = {
+            "ChunkStream": TPXFileReader(fname, self.queues['chunk']).make_process(),
+            "Converter": VMIConverter(self.queues['chunk'], self.queues['pixel'], self.queues['pulses'], self.queues['etof'], self.queues['itof']).make_process(),
+            "Clusterer": DBSCANClusterer(self.queues['pixel'], self.queues['clusters']).make_process(),
+            "Correlator": TriggerAnalyzer(self.queues['pulses'], (self.queues['etof'], self.queues['itof'], self.queues['clusters']), self.queues['t_pulse'], (self.queues['t_etof'], self.queues['t_itof'], self.queues['t_cluster'])).make_process(),
+            "Saver": SaveToH5(fname+".h5",
+                                {"etof": self.queues['t_etof'], "itof": self.queues['t_itof'], "pulse": self.queues['t_pulse'], "cluster": self.queues['t_cluster']},
+                              loud=True).make_process(),
+        }
+
+if __name__ == "__main__":
+    fname=r"J:\ctgroup\Edward\DATA\VMI\20241125\c2h4_p_5W"
+    pipeline = ClusterTest(fname)
+    run_pipeline(pipeline)
+#%%
---
+++ monitor_run.py
@@ -0,0 +1,7 @@
+from vmi_analysis.processing.pipelines import RunMonitorPipeline, run_pipeline
+import os
+
+if __name__ == '__main__':
+    fname=r"J:\ctgroup\Edward\DATA\VMI\20241125\c2h4_p_5W"
+    pipeline = RunMonitorPipeline(fname, cluster_processes=8)
+    run_pipeline(pipeline)
