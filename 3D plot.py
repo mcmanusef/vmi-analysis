0a4fa7d5cc5f0ad9547a48509193157b8a0287d2
mcman <mcmanusef@gmail.com>
2024-12-18 14:41:28 -0500

    added 3D plotting

--- 3D plot.py
+++ 3D plot.py
@@ -19,9 +19,9 @@ mask = (px ** 2 + py ** 2 + pz ** 2) < pmax ** 2
 px, py, pz = px[mask], py[mask], pz[mask]
 print(px.shape)
 
-hist, *e = np.histogramdd((px, py, pz), bins=256, range=((-pmax, pmax), (-pmax, pmax), (-pmax, pmax)))
+hist, *e = np.histogramdd((px, py, pz), bins=128, range=((-pmax, pmax), (-pmax, pmax), (-pmax, pmax)))
 
-hist_smooth = scipy.ndimage.gaussian_filter(hist, 2)
+hist_smooth = scipy.ndimage.gaussian_filter(hist, 0.75)
 hist = hist_smooth
 hist /= hist.max()
 hist = hist ** 0.3
@@ -29,7 +29,7 @@ print(hist.shape)
 
 data_to_plot = []
 #marching cubes
-for level in np.linspace(0.1, 0.9, 20):
+for level in np.linspace(0.1, 0.9, 7):
     verts, faces, _, _ = measure.marching_cubes(hist, level, spacing=(2 * pmax / 1024, 2 * pmax / 1024, 2 * pmax / 1024))
     faces = [(len(f), *f) for f in faces]
     faces = list(np.concatenate(faces))
@@ -39,6 +39,6 @@ plotter = pv.Plotter()
 # Create a PyVista mesh
 for level, verts, faces in data_to_plot:
     surf = pv.PolyData(verts, faces)
-    plotter.add_mesh(surf, color=cmap(level ** 1.5), opacity=level ** 3.5)
+    plotter.add_mesh(surf, color=cmap(level ** 1.5), opacity=level ** 2)
 # plotter.add_axes_at_origin()
 plotter.show()
