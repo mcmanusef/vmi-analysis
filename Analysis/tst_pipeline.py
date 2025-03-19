cea4932dd6b731e143c34e4c30a447a8b5a8938f
mcman <mcmanusef@gmail.com>
2025-03-19 13:48:41 -0400

    Moved post-analysis code
    Signed-off-by: mcman <mcmanusef@gmail.com>

---
+++ Analysis/tst_pipeline.py
@@ -0,0 +1,22 @@
+import logging
+import time
+
+import matplotlib.pyplot as plt
+
+from vmi_analysis.processing import pipelines
+
+if __name__ == "__main__":
+    pipeline = pipelines.LiveMonitorPipeline()
+    logger = logging.getLogger()
+
+    with pipeline:
+        print("Pipeline Initialized")
+        for process in pipeline.processes.values():
+            print(process.status())
+        pipeline.start()
+        print("Pipeline Started")
+        time.sleep(30)
+    print("Pipeline Exited")
+    plt.figure()
+    plt.imshow(pipeline.acc_frame)
+    plt.show()
--- start_ui.py
+++
@@ -1,5 +0,0 @@
-from vmi_analysis.acquisition_ui import MainApp
-
-if __name__ == "__main__":
-    app = MainApp()
-    app.mainloop()
